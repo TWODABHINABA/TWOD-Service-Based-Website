162a4dd1d55c27ce68d252762efa9920a55183ab
Abhinaba Das <tstatai02@gmail.com>
2025-06-30 22:17:21 +0530

    sample

--- Backend/utils/db.js
+++ Backend/utils/db.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose");
 require("dotenv").config();
-mongoose.connect(process.env.DB_URL,{
+mongoose.connect(process.env.DB_URL || "mongodb+srv://tstatai02:iamf00L@twod.bh7wxgv.mongodb.net/",{
 });
 
 const db=mongoose.connection
